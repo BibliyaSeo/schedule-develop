0e636fa34cd8451c49e31ce132a613a0aca9df4f
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-08-08 20:19:37 +0900

    feat: 에러 코드 추가

--- src/main/java/org/example/scheduledevelop/common/exception/ErrorCode.java
+++ src/main/java/org/example/scheduledevelop/common/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     INVALID_INPUT("VAL-001", "입력값이 유효하지 않습니다"),
-    DUPLICATE_USER("USR-001", "이미 가입된 사용자입니다");
+    DUPLICATE_USER("USR-001", "이미 가입된 사용자입니다"),
+    USER_NOT_FOUND("USR-002", "존재하지 않는 회원입니다"),
+    POST_NOT_FOUND("POST-001", "존재하지 않는 게시글입니다");
 
     private final String code;
     private final String message;
