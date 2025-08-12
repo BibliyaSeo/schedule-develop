b2e78851f5f367e51ab21c061d68fb9046089c7e
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-08-12 21:17:42 +0900

    feat: 에러 코드 추가, 일정 유저 아이디 체크

--- src/main/java/org/example/scheduledevelop/common/exception/ErrorCode.java
+++ src/main/java/org/example/scheduledevelop/common/exception/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
     INVALID_INPUT("VAL-001", "입력값이 유효하지 않습니다"),
     DUPLICATE_USER("USR-001", "이미 가입된 사용자입니다"),
     USER_NOT_FOUND("USR-002", "존재하지 않는 회원입니다"),
-    POST_NOT_FOUND("POST-001", "존재하지 않는 게시글입니다");
+    POST_NOT_FOUND("POST-001", "존재하지 않는 게시글입니다"),
+    COMMENT_NOT_FOUND("COMMENT-001", "존재하지 않는 댓글입니다");
 
     private final String code;
     private final String message;
--- src/main/java/org/example/scheduledevelop/schedule/dto/UpdateScheduleRequestDto.java
+++ src/main/java/org/example/scheduledevelop/schedule/dto/UpdateScheduleRequestDto.java
@@ -1,16 +1,13 @@
 package org.example.scheduledevelop.schedule.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateScheduleRequestDto {
-    @NotBlank(message = "제목은 필수 입력 사항입니다.")
     @Size(max = 10, message = "제목은 10자 이내여야 합니다.")
     private final String title;
 
-    @NotBlank(message = "내용은 필수 입력 사항입니다.")
     @Size(max = 100, message = "내용은 10자 이내여야 합니다.")
     private final String contents;
 
