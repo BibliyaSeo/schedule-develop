8d7e7579910e69f3cc7a0b8751bb64d12f9e26bd
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-08-14 14:35:28 +0900

    feat: email unique 추가

--- src/main/java/org/example/scheduledevelop/users/entity/User.java
+++ src/main/java/org/example/scheduledevelop/users/entity/User.java
@@ -16,7 +16,7 @@ public class User extends BaseEntity {
 
     @Column(name = "username", nullable = false)
     private String username;
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
     @Column(name = "password", nullable = false)
     private String password;
